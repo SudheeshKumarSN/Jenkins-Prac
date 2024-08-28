00328c2974dbb9c57945c3bfb65995e56e711760
SudheeshKumarSN <gowdrumanu130@gmail.com>
2024-08-28 23:30:24 +0530

    Update

---
+++ Jenkins_parameter.groovy
@@ -0,0 +1,16 @@
+pipeline{
+    agent any
+    parameters{ string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'The Git branch to build') }
+    environment{
+        REPO_URL = 'https://github.com/ElevenDevOps/eleven-devops-maven-repo.git'
+    }
+    stages{
+        stage("checkout"){
+            steps{
+                git branch: "${params.BRANCH_NAME}" , url: "${env.REPO_URL}"
+                echo "${params.BRANCH_NAME}"
+                ls -lrt
+            }
+        }
+    }
+}
